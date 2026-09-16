116a318971f21754fdd36470ea1783f400ec3cf4
Adrian Statescu <mergesortv@gmail.com>
2026-09-16 10:44:17 +0300

    fixed

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(),
-    base: '/react-vite-portofolio/',
+    tailwindcss(),  
   ],
+  base: '/react-vite-portofolio/'
 })
