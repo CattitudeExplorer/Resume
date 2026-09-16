a3274dac55f446c2fa2c24e2f0e6d5eb2f04e4f2
Adrian Statescu <mergesortv@gmail.com>
2026-09-16 12:26:14 +0300

    hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -25,7 +25,7 @@ function Hero() {
           </h1>
 
           <h2 className="text-2xl md:text-4xl font-semibold text-gray-300 mt-4">
-            React Developer & Full Stack Enthusiast
+            React Developer & Backend Enthusiast
           </h2>
 
           <p className="text-gray-400 mt-6 leading-8 text-lg">
