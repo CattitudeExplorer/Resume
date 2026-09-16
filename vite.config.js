1b891eac7f664a9f3be647fdae7e101a3beee0f5
Adrian Statescu <mergesortv@gmail.com>
2026-09-16 11:45:47 +0300

    updated

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],  
-})
+export default defineConfig(({ mode }) => ({
+  plugins: [react(), tailwindcss()],
+  base: mode === 'gh-pages' ? '/react-vite-portofolio/' : '/',
+}))
